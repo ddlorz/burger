b3397534b2baa03e359e4572ba1c8010ebf4f8b7
djrloria <djrloria@gmail.com>
2017-06-12 20:23:30 -0500

    updated connection.js with connection to JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,14 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+var xconnection = mysql.createConnection({
+    host: 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'k52gernmnarmb4p9',
+    password: 'r1qe0olus7kt5eg2',
+    database: "burgers_db"
+});
+
 connection.connect(function(err) {
     if (err) {throw err;}
     console.log("Connected as ID + " + connection.threadId);
