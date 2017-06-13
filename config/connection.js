47f9a2326e7287293693a6b585c7b5f3582c163d
djrloria <djrloria@gmail.com>
2017-06-12 20:43:44 -0500

    another heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 
-var xconnection = mysql.createConnection({
+/*var xconnection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -8,7 +8,7 @@ var xconnection = mysql.createConnection({
     database: "burgers_db"
 });
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
     user: 'k52gernmnarmb4p9',
