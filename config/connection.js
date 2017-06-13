73fd1bf2a3378c63ffc57273d05741919aebe388
djrloria <djrloria@gmail.com>
2017-06-12 21:54:01 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,18 @@
 var mysql = require('mysql');
 
-/*var xconnection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection =  mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
     database: "burgers_db"
-});
-
-var connection = mysql.createConnection({
-    host: 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    port: 3306,
-    user: 'k52gernmnarmb4p9',
-    password: 'r1qe0olus7kt5eg2',
-    database: "burgers_db"
-});*/
-
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+};
 
 connection.connect(function(err) {
     if (err) {throw err;}
